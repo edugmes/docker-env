3c32fc8c7d9f2c9e35a35cbc6e162c13ae7a2dbf
Eduardo Gomes <edu.gmes0@gmail.com>
2021-12-15 12:25:35 -0300

    fix(cassandra container): shell script endless loop Added 'if exists' to cql query so that 'until' command would exit the loop. Otherwise the query would still evaluate to 'false' even after keyspace was created.

--- containers/django_cassandra/cassandra/cassandra_init.sh
+++ containers/django_cassandra/cassandra/cassandra_init.sh
@@ -1,8 +1,9 @@
-cql="create keyspace posts_k with replication = {'class': 'SimpleStrategy', 'replication_factor': 2};"
+cas_query="create keyspace if not exists posts_k with replication = {'class': 'SimpleStrategy', 'replication_factor': 2};"
 
-until echo $cql; do
-	now=$(date + "%T")
-	echo "$now - cqlsh: Node still unavailable, will retry another time"
+until echo $cas_query | cqlsh
+do
+	now=$(date +%T)
+	echo "[$now INIT CQLSH]: Node still unavailable, will retry another time"
 	sleep 2
 done &
 
